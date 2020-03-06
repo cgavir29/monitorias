4a9d8a3c95d0dcb96c0a2111adc2c5b3aa1e11ab
cgavir29 <35697795+cgavir29@users.noreply.github.com>
2020-03-05 23:03:59 -0500

    Update covid.py

--- covid.py
+++ covid.py
@@ -1,24 +1,28 @@
 while True:
   try:
-    # Me ingresan el nombre del fichero
+    # Me ingresan el nombre del fichero.
     file_name = input('Ingrese el nombre del fichero: ')
-    # Intento abrir el fichero
+    
+    # Intento abrir el fichero.
     f = open(file_name, 'r')
-    # Lo guardo en una lista si funciona, sino se va en a la excepcion
+    
+    # Lo guardo en una lista si funciona, sino va a la excepción.
     file_contents = f.readlines()
-    # Cierro el archivo
+    
+    # Cierro el fichero.
     f.close()
 
-    # Total de infectados
+    # Total de infectados.
     total_infected = 13149 # len(file_contents)
-    # Inicializo los contadores
+    
+    # Inicializo los contadores.
     total_china = 0
     total_italy = 0
     total_japan = 0
 
-    # Itero sobre los contenidos del archivo
+    # Itero sobre los contenidos del fichero.
     for country in file_contents:
-      # Voy sumando al respectivo pais
+      # Voy sumando al respectivo país.
       if country == 'China\n':
         total_china += 1
       elif country == 'Italy\n':
@@ -26,24 +30,25 @@ while True:
       elif country == 'Japan\n':
         total_japan += 1
 
-    # Abro el archivo
+    # Abro el fichero.
     estadisticas = open('estadisticas.txt', 'w')
-    # Copio en el archivo
+    
+    # Copio en el fichero.
     estadisticas.write('El numero de infectados de China es de ' + str(total_china) + 
     ' y su porcentaje del total es de ' + str((total_china / total_infected) * 100) + '\n')
     estadisticas.write('El numero de infectados de Italy es de ' + str(total_italy) + 
     ' y su porcentaje del total es de ' + str((total_italy / total_infected) * 100) + '\n')
     estadisticas.write('El numero de infectados de Japan es de ' + str(total_japan) + 
     ' y su porcentaje del total es de ' + str((total_japan / total_infected) * 100) + '\n')
-    # Cierro el archivo
+    
+    # Cierro el fichero.
     estadisticas.close()
-
+  
+    # Imprimo la suma de infectados entre los 3 países.
     print('El total de infectados de China, Italy, y Japan es: ' + 
     str(total_china + total_italy + total_japan))
 
-    # Si termino el programa salgo del while
+    # Si termino el programa salgo del while.
     break
   except FileNotFoundError:
-    print('Error: el fichero no existe')
-
-
+    print('Error: el fichero no existe.')
